34828492fac328b50561744f2b8509e97e116106
ilanbenyos <ilan_ben@hotmal.com>
2019-05-27 12:24:06 +0300

    migrating to heroku

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-let baseApi = "http://localhost:3001/v1";
+
+// let baseApi = "http://localhost:5000/v1";//https://dry-badlands-98330.herokuapp.com
+let baseApi = "https://dry-badlands-98330.herokuapp.com/v1";//https://dry-badlands-98330.herokuapp.com
 
 export { baseApi };
